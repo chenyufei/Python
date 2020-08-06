2f96f3c004d6fd7489e1dfe2482291805019ab55
chenyufei <251813306@qq.com>
2020-08-06 15:54:43 +0800

    规范格式及

--- MultiCsvToSheet/mergecsvtosheet.py
+++ MultiCsvToSheet/mergecsvtosheet.py
@@ -11,6 +11,11 @@ import threading
 import json
 
 class Dest_Data():
+    """
+    完成GSP发货测试数据的合并
+    针对每种传感器数据，只需要在程序目录src下创建对应的保存传感数据的文件夹
+    然后把对应的数据文件拷到对应的目录下面即可
+    """
     def __init__(self):
         self.foldnames = []
         self.cur_dir = os.getcwd()
@@ -19,7 +24,12 @@ class Dest_Data():
         self.deveuis = []
         self.deveui_file = self.cur_dir +"\\deveui\\deveui.txt"
 
-    def readtext(self,filename):
+    def readtext(self, filename):
+        """
+        读取txt文件，并把每 行内行转列表形式返回
+        :param filename: 待读txt文件名
+        :return: 数据的行列表
+        """
         lines = []
         with open(filename, 'r') as file_to_read:
             while True:
@@ -34,12 +44,16 @@ class Dest_Data():
         self.deveuis = self.readtext(self.deveui_file)
 
     def get_fold(self):
+        """
+        获取src目录下面所有子目录（不递归查询）
+        :return:无
+        """
         for root, dirs, files in os.walk(self.src_dir):
             self.foldnames = dirs
-            print('sub_dirs:', self.foldnames)
+            print('excel sheet name:', self.foldnames)
             break
 
-    #功能函数
+    #  功能函数
     def generate_temp_file(self):
         for item in self.foldnames:
             cur_dir = self.src_dir+item+"\\"
@@ -49,7 +63,7 @@ class Dest_Data():
             for temp in filenames:
                 print(temp)
                 count += self.deal_file(temp, item)
-            print(item +":"+str(count))
+            print(item + ":" + str(count))
 
     def remove_dst_file(self, filetype='.csv'):
         dstfile = self.file_name(self.dst_dir, filetype)
@@ -83,8 +97,8 @@ class Dest_Data():
     def generate_file(self):
         self.get_fold()  # 获取子目
         self.get_deveui()  # 获取需要生成的deveui列表
-        self.generate_temp_file() # 根据获取的子目录获取子目录下的文件
-        self.merge_file()
+        self.generate_temp_file()  # 根据获取的子目录获取子目录下的文件
+        self.merge_file()  # 根据上面的临时文件生成最终的表格数据
 
     @staticmethod
     def file_name(file_dir, filetype='.csv'):
@@ -125,11 +139,11 @@ class Dest_Data():
         checkthread.start()
         checkthread.join()
 
-
-    #获取当前时间
+    # 获取当前时间
     def get_current_time(self):
         current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         return current_time
+
 
 def main():
     ZMJ_PORTAL = Dest_Data()
@@ -143,4 +157,4 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+    main()
